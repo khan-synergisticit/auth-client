e213fdc9cb3b0ae086548cb4237c748a7d5f37e2
american-worx <khan@americanworx.com>
2024-10-18 07:23:43 -0400

    auth

--- src/main/java/com/americanworx/authclient/config/Constants.java
+++ src/main/java/com/americanworx/authclient/config/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
     public static final String CLIENT_CREDENTIALS = "shopping:donkey";
 
     public static final String OAUTH_TOKEN_URL = "http://localhost:8040/oauth2/token";
-    public static final String SHOP_URL ="http://192.168.1.129:8080";
+    public static final String SHOP_URL ="http://192.168.1.253:8080";
 }
