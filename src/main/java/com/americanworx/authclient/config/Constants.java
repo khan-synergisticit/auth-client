da36d978e305b90b7853be13acf44d1f481bfdd3
american-worx <khan@americanworx.com>
2024-10-25 06:32:56 -0400

    url

--- src/main/java/com/americanworx/authclient/config/Constants.java
+++ src/main/java/com/americanworx/authclient/config/Constants.java
@@ -1,7 +1,14 @@
 package com.americanworx.authclient.config;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class Constants {
+    @Value("${url.shop}")
+    private static String shop;
+    @Value("${url.auth-server}")
+    private static String auth_server;
+
     public static final String CLIENT_CREDENTIALS = "shopping:donkey";
-    public static final String AUTH_SERVER_URL= "http://192.168.1.75";
-    public static final String SHOP_URL ="http://192.168.1.69";
+    public static final String AUTH_SERVER_URL= auth_server;
+    public static final String SHOP_URL =shop;
 }
