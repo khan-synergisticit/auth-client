ed2250b829366508463899a878f9fb2fca1bacb2
american-worx <khan@americanworx.com>
2024-10-29 20:29:34 -0400

    url

--- src/main/java/com/americanworx/authclient/config/Constants.java
+++ src/main/java/com/americanworx/authclient/config/Constants.java
@@ -2,8 +2,8 @@ package com.americanworx.authclient.config;
 
 public class Constants {
     public static final String CLIENT_CREDENTIALS = "shopping:donkey";
-    public static final String AUTH_SERVER_URL= "http://192.168.1.75";
-    public static final String AUTH_CLIENT_URL = "http://192.168.1.76";
-    public static final String SHOP_URL_BASE ="192.168.1.69";
+    public static final String AUTH_SERVER_URL= "http://192.168.1.129";
+    public static final String AUTH_CLIENT_URL = "http://192.168.1.110";
+    public static final String SHOP_URL_BASE ="192.168.1.111";
     public static final String SHOP_URL ="http://" + SHOP_URL_BASE;
 }
