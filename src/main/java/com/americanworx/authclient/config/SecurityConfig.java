ff34a428336a125e1658645eb4ab89dea9c74473
american-worx <khan@americanworx.com>
2024-11-01 13:16:46 -0400

    cookie

--- src/main/java/com/americanworx/authclient/config/SecurityConfig.java
+++ src/main/java/com/americanworx/authclient/config/SecurityConfig.java
@@ -123,7 +123,7 @@ public class SecurityConfig {
 //                    cookie.setMaxAge(duration.toSecondsPart());
 //                    response.addCookie(cookie);
 //                    System.out.println("cookie: " + cookie.getValue());
-                    response.setContentType("application/x-www-form-urlencoded");
+                    response.setContentType("application/json");
                     response.addHeader("Cookie", token.getTokenValue());
 
                     redirectStrategy.sendRedirect(request, response, Constants.SHOP_URL + ":8080/loggedIn");
