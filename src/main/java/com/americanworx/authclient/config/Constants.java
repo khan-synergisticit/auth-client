18abe5cd68bf7e144a167b62ca54785bdf493f57
american-worx <khan@americanworx.com>
2024-11-12 10:52:07 -0500

    kube url

--- src/main/java/com/americanworx/authclient/config/Constants.java
+++ src/main/java/com/americanworx/authclient/config/Constants.java
@@ -2,8 +2,8 @@ package com.americanworx.authclient.config;
 
 public class Constants {
     public static final String CLIENT_CREDENTIALS = "shopping:donkey";
-    public static final String AUTH_SERVER_URL= "http://192.168.1.129";
-    public static final String AUTH_CLIENT_URL = "http://192.168.1.111";
-    public static final String SHOP_URL_BASE ="192.168.1.110";
+    public static final String AUTH_SERVER_URL= "http://10.96.0.1";
+    public static final String AUTH_CLIENT_URL = "http://10.96.0.2";
+    public static final String SHOP_URL_BASE ="10.96.0.4";
     public static final String SHOP_URL ="http://" + SHOP_URL_BASE;
 }
